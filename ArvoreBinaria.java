e5b9c49cc65cdc5c348bb74e3b14551e0fa47b70
pedrogmo <42345539+pedrogmo@users.noreply.github.com>
2019-09-10 11:51:53 -0300

    Árvore + Relatório

---
+++ ArvoreBinaria.java
@@ -0,0 +1,97 @@
+public class ArvoreBinaria<T extends Comparable<T>>{
+	private class No<T extends Comparable<T>>{
+		private T dado;
+		public No<T> esquerda, direita;
+
+		public No(
+			T dado, 
+			No<T> esquerda,
+			No<T> direita) throws Exception
+		{
+
+		}
+
+		public No(
+			No<T> modelo)
+		{
+
+		}
+
+		public Object clone(){
+
+		}
+
+		public T getDado(){
+
+		}
+
+		public void setDado(
+			T dado) throws Exception
+		{
+
+		}
+
+		public String toString(){
+
+		}
+
+		public int hashCode(){
+
+		}
+
+		public boolean equals(
+			Object obj)
+		{
+			
+		}
+	}
+
+	private No<T> raiz;
+	private int qtd=0;
+
+	public ArvoreBinaria(){
+
+	}
+
+	public ArvoreBinaria(
+		ArvoreBinaria modelo)
+	{
+
+	}
+
+	public Object clone(){
+
+	}
+
+	public void adicionar(
+		T dado) throws Exception
+	{
+
+	}
+
+	public void remover(
+		T dado) throws Exception
+	{
+
+	}
+
+	public T buscar(
+		T dado) throws Exception
+	{
+
+	}
+
+	public String toString(){
+
+	}
+
+	public int hashCode(){
+
+	}
+
+	public boolean equals(
+		Object obj)
+	{
+
+	}
+}
