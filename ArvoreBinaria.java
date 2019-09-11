ee77f1e743c9be8ef7b56e12f391f51e94c1bebb
pedrogmo <42345539+pedrogmo@users.noreply.github.com>
2019-09-11 13:42:38 -0300

    Árvore
    Falta adicionar, remover e métodos AVL

--- ArvoreBinaria.java
+++ ArvoreBinaria.java
@@ -1,24 +1,14 @@
 import java.lang.reflect.*;
 
-public class ArvoreBinaria<T extends Comparable<T>> implements Cloneable{
-	private class No<T extends Comparable<T>> implements Cloneable, Comparable<T>{
-		private T dado;
-		public No<T> esquerda, direita;
-		private int altura=0;
-
-		private T cloneDeT(T t)
-		{
-			T ret = null;
-			try{
-				Class<?> classe = t.getClass();
-				Class<?>[] tiposFormais = null;
-				Method metodo = classe.getMethod("clone", tiposFormais);
-				Object[] pametrosReais = null;
-				ret = (T) metodo.invoke(t, pametrosReais);
-			}
-			catch(Exception a){}
-			return ret;
-		}
+public class ArvoreBinaria<T extends Comparable<T>> 
+	implements Cloneable
+{
+	private class No<T extends Comparable<T>> 
+		implements Cloneable, Comparable<No<T>>
+	{
+		private T dado = null;
+		public No<T> esquerda = null, direita = null;
+		private int altura = 0;
 
 		public No(
 			T info, 
@@ -29,14 +19,14 @@ public class ArvoreBinaria<T extends Comparable<T>> implements Cloneable{
 			this.setDado(info);
 			this.esquerda = esq;
 			this.direita = dir;
-			this.altura = alt;
+			this.setAltura(alt);
 		}
 
 		public No(
 			No<T> modelo)
 		{
 			if (modelo.dado instanceof Cloneable)
-				this.dado = cloneDeT(modelo.dado);
+				this.dado = this.cloneDeT(modelo.dado);
 			else
 				this.dado = modelo.dado;
 			if (modelo.esquerda != null)
@@ -49,13 +39,13 @@ public class ArvoreBinaria<T extends Comparable<T>> implements Cloneable{
 		public Object clone(){
 			No<T> ret = null;
 			try{ ret = new No<T>(this); }
-			catch(Exception exc){  }
+			catch(Exception exc){}
 			return ret;
 		}
 
 		public T getDado(){
 			if (this.dado instanceof Cloneable)
-				return cloneDeT(this.dado);
+				return this.cloneDeT(this.dado);
 			return this.dado;
 		}
 
@@ -63,11 +53,23 @@ public class ArvoreBinaria<T extends Comparable<T>> implements Cloneable{
 			T info) throws Exception
 		{
 			if (info==null)
-				throw new NullPointerException("Dado nulo");
+				throw new NullPointerException("No<T>: Dado nulo");
 			if (info instanceof Cloneable)
-				this.dado = cloneDeT(info);
+				this.dado = this.cloneDeT(info);
 			else
 				this.dado = info;
+		}
+
+		public int getAltura(){
+			return this.altura;
+		}
+
+		public void setAltura(
+			int alt) throws Exception
+		{
+			if (alt < 0)
+				throw new IllegalArgumentException("No<T>: Altura negativa");
+			this.altura = alt;
 		}
 
 		public boolean isFolha(){
@@ -139,56 +141,156 @@ public class ArvoreBinaria<T extends Comparable<T>> implements Cloneable{
 		public int compareTo(
 			No<T> outro)
 		{
-			return this.dado.compareTo(outro.dado);
+			return this.compT(this.dado, outro.dado);
+		}
+
+		private T cloneDeT(T t)
+		{
+			T ret = null;
+			try{
+				Class<?> classe = t.getClass();
+				Class<?>[] tiposFormais = null;
+				Method metodo = classe.getMethod("clone", tiposFormais);
+				Object[] pametrosReais = null;
+				ret = (T) metodo.invoke(t, pametrosReais);
+			}
+			catch(Exception erro){}
+			return ret;
+		}
+
+		private int compT(
+			T t0,
+			T t1)
+		{
+			int ret = 0;
+			try
+			{
+				Class<?> classe = t0.getClass();
+				Class<?>[] tiposParametrosFormais = new Class<?>[1];
+				tiposParametrosFormais[0] = classe;
+				Method metodo = classe.getMethod("compareTo", tiposParametrosFormais);
+				Object[] parametrosReais = new Object[1];
+				parametrosReais[0] = t1;
+				ret = (int) metodo.invoke(t0,parametrosReais);
+			}
+			catch (Exception erro){}
+			return ret;
 		}
 	}
 
-	private No<T> raiz;
-	private int qtd=0;
+	private No<T> raiz = null;
+	private int qtd = 0;
 
-	public ArvoreBinaria(){
-
-	}
+	public ArvoreBinaria()
+	{}
 
 	public ArvoreBinaria(
 		ArvoreBinaria modelo)
 	{
-
+		this.qtd = modelo.qtd;
+		if (modelo.raiz != null)
+			this.raiz = new No<T>(modelo.raiz);
 	}
 
 	public Object clone(){
+		ArvoreBinaria<T> ret = null;
+		try{ ret = new ArvoreBinaria<T>(this); }
+		catch(Exception erro){}
+		return ret;
+	}
 
+	public int getQuantidade(){
+		return this.qtd;
+	}
+
+	public boolean isVazia(){
+		return this.raiz == null;
 	}
 
 	public void adicionar(
-		T dado) throws Exception
+		T info) throws Exception
 	{
 
 	}
 
 	public void remover(
-		T dado) throws Exception
+		T info) throws Exception
 	{
 
 	}
 
 	public T buscar(
-		T dado) throws Exception
+		T info) throws Exception
 	{
+		if (this.isVazia())
+			return null;
+		No<T> noProcurado = 
+			new No<T>(
+				info,
+				null,
+				null,
+				0);
+		return buscar(
+			noProcurado, 
+			this.raiz);
+	}
+	private T buscar(
+		No<T> procurado,
+		No<T> atual)
+	{
+		int comp = procurado.compareTo(atual);
+		if (comp == 0)
+			return atual.getDado();
 
+		else if (comp < 0){
+			if (atual.esquerda == null)
+				return null;
+			return buscar(
+				procurado, 
+				atual.esquerda);
+		}
+
+		if (atual.direita == null)
+			return null;
+		return buscar(
+			procurado, 
+			atual.direita);
 	}
 
 	public String toString(){
-
+		if (this.isVazia())
+			return "Arvore vazia";
+		return this.raiz.toString();
 	}
 
 	public int hashCode(){
+		int ret = 1;
 
+		ret += 2 * new Integer(this.qtd).hashCode();
+		if (!this.isVazia())
+			ret += 2 * this.raiz.hashCode();
+
+		return ret;
 	}
 
 	public boolean equals(
 		Object obj)
 	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (this.getClass() != obj.getClass())
+			return false;
 
+		ArvoreBinaria a = (ArvoreBinaria) obj;
+
+		if (this.qtd != a.qtd)
+			return false;
+
+		if (this.isVazia())
+			return true;
+
+		return this.raiz.equals(a.raiz);
 	}
 }
---
+++ Teste.java
@@ -0,0 +1,8 @@
+public class Teste{
+	public static void main(
+		String[] args)
+	{
+		ArvoreBinaria ab = new ArvoreBinaria();
+		System.out.println(ab.toString());
+	}
+}
