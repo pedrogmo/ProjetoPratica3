67af27d040f61a0720227393f20a4d25d93cd868
GustavoHMeira <42345835+GustavoHMeira@users.noreply.github.com>
2019-10-05 17:26:31 -0300

    despreze, te explico o motivo

---
+++ AppAndroidFotovoltaica/app/src/main/java/com/example/appandroidfotovoltaica/Cliente.java
@@ -0,0 +1,7 @@
+package com.example.appandroidfotovoltaica;
+
+public class Cliente {
+    private String nome;
+    private String email;
+    private int cnpj; // é int pois já haverão os pontos e traços
+}
