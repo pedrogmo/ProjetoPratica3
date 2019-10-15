714683ab053aa6548e6db25753d3bf9ba36f12e4
GustavoHMeira <42345835+GustavoHMeira@users.noreply.github.com>
2019-10-15 00:30:24 -0300

    só testei o node mesmo

--- AppAndroidFotovoltaica/app/src/main/java/com/example/appandroidfotovoltaica/Enderecos.java
+++ AppAndroidFotovoltaica/app/src/main/java/com/example/appandroidfotovoltaica/Enderecos.java
@@ -2,8 +2,8 @@ package com.example.appandroidfotovoltaica;
 
 public class Enderecos
 {
-    public static final String POST_CLIENTE = "http://177.220.18.82:3000/insert_cliente";
-    public static final String GET_CLIENTES = "http://177.220.18.82:3000/clientes";
-    public static final String PATCH_CLIENTES = "http://177.220.18.82:3000/update_cliente";
-    public static final String DELETE_CLIENTES = "http://177.220.18.82:3000/delete_cliente";
+    public static final String POST_CLIENTE = "http://192.168.1.119:3000/insert_cliente";
+    public static final String GET_CLIENTES = "http:// 192.168.1.119:3000/clientes";
+    public static final String PATCH_CLIENTES = "http://192.168.1.119:3000/update_cliente";
+    public static final String DELETE_CLIENTES = "http://192.168.1.119:3000/delete_cliente";
 }
