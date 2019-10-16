20d22c429c95deec38f2a5e9c2364914c0059823
GustavoHMeira <cc18015@g.unicamp.br>
2019-10-16 08:46:22 -0300

    ta lá

--- AppAndroidFotovoltaica/app/src/main/java/com/example/appandroidfotovoltaica/ui/login/Login.java
+++ AppAndroidFotovoltaica/app/src/main/java/com/example/appandroidfotovoltaica/ui/login/Login.java
@@ -1,31 +1,47 @@
 package com.example.appandroidfotovoltaica.ui.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.example.appandroidfotovoltaica.R;
 import com.example.appandroidfotovoltaica.ui.adicionarcliente.AdicionarClienteFragment;
+import com.example.appandroidfotovoltaica.ui.home.HomeFragment;
+import com.example.appandroidfotovoltaica.ui.home.HomeViewModel;
+import com.example.appandroidfotovoltaica.ui.principalClientes.PrincipalClientesFragment;
 
-public class Login extends AppCompatActivity {
+public class Login extends Fragment {
+    private LoginViewModel loginViewModel;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        Button btnLogar = findViewById(R.id.btnLogar);
+        loginViewModel =
+                ViewModelProviders.of(this).get(LoginViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_login, container, false);
+
+        final Button btnLogar = root.findViewById(R.id.btnLogar);
         btnLogar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FragmentTransaction fragmentTransaction = getFragmentManager()
-                fragmentTransaction.replace(R.id.fragment_principalClientes, new AdicionarClienteFragment());
+                FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_login, new HomeFragment());
                 fragmentTransaction.commit();
 
 
             }
         });
+        return root;
     }
+
 }
