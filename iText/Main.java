f01598a280d4f37b3b29d276a2aa6635976fb597
PEDRO GOMES MOREIRA <u18174@academico.cotuca.unicamp.br>
2019-10-17 12:52:06 -0300

    Movi arquivos do arduino para pasta
    + criação da pasta iText

---
+++ iText/Main.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main
+{
+	public static void main(
+		String[] args)
+	{
+		try
+		{
+			BufferedReader teclado = new BufferedReader(
+				new InputStreamReader(
+					System.in));
+
+			
+		}
+		catch(Exception exc)
+		{
+			System.err.println(exc.getMessage());
+		}
+	}
+}
