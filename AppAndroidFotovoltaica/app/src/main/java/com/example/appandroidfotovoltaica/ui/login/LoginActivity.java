75a3ca3113bae86a1dbd3fb78fc8318777101245
pedrogmo <42345539+pedrogmo@users.noreply.github.com>
2019-10-21 22:12:15 -0300

    Tentativa falha de árvore

--- AppAndroidFotovoltaica/app/src/main/java/com/example/appandroidfotovoltaica/ui/login/LoginActivity.java
+++ AppAndroidFotovoltaica/app/src/main/java/com/example/appandroidfotovoltaica/ui/login/LoginActivity.java
@@ -9,12 +9,14 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.appandroidfotovoltaica.ArvoreBinaria;
 import com.example.appandroidfotovoltaica.Enderecos;
 import com.example.appandroidfotovoltaica.MainActivity;
 import com.example.appandroidfotovoltaica.MyTask;
@@ -26,15 +28,35 @@ import android.content.Intent;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private ArvoreBinaria<Usuario> arvoreUsuarios;
+    private EditText etEmailLogin, etSenhaLogin;
+    private Button btnLogar;
+    private TextView tvCadastrarLogin;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final EditText etEmailLogin = findViewById(R.id.etEmailLogin);
-        final EditText etSenhaLogin = findViewById(R.id.etSenhaLogin);
-        final Button btnLogar = findViewById(R.id.btnLogar);
-        final TextView tvCadastrarLogin = findViewById(R.id.tvCadastrarLogin);
+        etEmailLogin = findViewById(R.id.etEmailLogin);
+        etSenhaLogin = findViewById(R.id.etSenhaLogin);
+        btnLogar = findViewById(R.id.btnLogar);
+        tvCadastrarLogin = findViewById(R.id.tvCadastrarLogin);
+
+        MyTask task = new MyTask(Usuario[].class);
+        task.execute(Enderecos.GET_USUARIOS);
+        while (task.isTrabalhando()) ;
+        for(Usuario u : (Usuario[]) task.getDados())
+        {
+            try
+            {
+                arvoreUsuarios.adicionar(u);
+            }
+            catch(Exception exc)
+            {
+                Log.d("ERRO", exc.getMessage());
+            }
+        }
 
         tvCadastrarLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,40 +71,25 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                if(etEmailLogin.getText().toString().equals(""))
+                final String email =  etEmailLogin.getText().toString().trim();
+                final String senha = etSenhaLogin.getText().toString().trim();
+                if(email.equals("") || senha.equals(""))
                 {
                     Toast.makeText(getApplicationContext(),"Digite os dados de login", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                MyTask task = new MyTask(Usuario[].class);
-                task.execute(Enderecos.GET_USUARIOS + "_email/" + etEmailLogin.getText().toString());
-                while (task.isTrabalhando()) ;
-                //ArvoreBinaria<String> arvoreBinaria = new ArvoreBinaria<String>();
-                Usuario[] resultUsuarios = (Usuario[]) task.getDados();
-                    /*for (int i = 0; i < resultUsuarios.length; i++)
-                    {
-                        try{
-                            arvoreBinaria.adicionar(resultUsuarios[i].getEmail());
-                        }
-                        catch (Exception e){}
-                    }
-                    try{
-                        if (arvoreBinaria.buscar(email) != null)
-                        {
-                            Toast.makeText(
-                                    getActivity().getApplicationContext(),
-                                    "Email já cadastrado",
-                                    Toast.LENGTH_SHORT).show();
-                            return;
-                        }
-                    }
-                    catch(Exception e){Toast.makeText(getActivity().getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT);}
-                    */
+                Usuario busca = null;
+                try
+                {
+                    busca = arvoreUsuarios.buscar(new Usuario(email)); //ERRO AQUI
+                }
+                catch(Exception exc)
+                {
+                    Log.d("ERRO", exc.getMessage());
+                }
 
-
-
-                if (resultUsuarios.length <= 0)
+                if (busca == null)
                 {
                     Toast.makeText(
                             getApplicationContext(),
@@ -90,25 +97,51 @@ public class LoginActivity extends AppCompatActivity {
                             Toast.LENGTH_SHORT).show();
                     return;
                 }
-                else
-                {
-                    boolean loginCorreto = false;
-                    for (int i = 0; i < resultUsuarios.length; i++)
-                    {
-                        if (resultUsuarios[i].getSenha() == etSenhaLogin.getText().toString())
-                            loginCorreto = true;
-                    }
-                    if (loginCorreto == false){
-                        Toast.makeText(getApplicationContext(),"Dados incorretos",Toast.LENGTH_SHORT).show();
-                    }
-                }
 
-                Toast.makeText(getApplicationContext(),"Login efetuado com sucesso",Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+                if (busca.getSenha().equals(senha))
+                {
+                    Toast.makeText(getApplicationContext(),"Login efetuado com sucesso",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+                else
+                    Toast.makeText(getApplicationContext(),"Senha incorreta",Toast.LENGTH_SHORT).show();
             }
         });
-
-
     }
 }
+
+/*
+    Código anterior - vetor de usuários
+
+    MyTask task = new MyTask(Usuario[].class);
+    task.execute(Enderecos.GET_USUARIOS + "_email/" + etEmailLogin.getText().toString());
+    while (task.isTrabalhando()) ;
+    Usuario[] resultUsuarios = (Usuario[]) task.getDados();
+
+    if (resultUsuarios.length <= 0)
+    {
+        Toast.makeText(
+            getApplicationContext(),
+            Toast.LENGTH_SHORT).show();
+        return;
+    }
+
+    else
+    {
+        boolean loginCorreto = false;
+        for (int i = 0; i < resultUsuarios.length; i++)
+        {
+            if (resultUsuarios[i].getSenha() == etSenhaLogin.getText().toString())
+                loginCorreto = true;
+        }
+        if (loginCorreto == false){
+            Toast.makeText(getApplicationContext(),"Dados incorretos",Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+    Toast.makeText(getApplicationContext(),"Login efetuado com sucesso",Toast.LENGTH_SHORT).show();
+    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+    startActivity(intent);
+*/
