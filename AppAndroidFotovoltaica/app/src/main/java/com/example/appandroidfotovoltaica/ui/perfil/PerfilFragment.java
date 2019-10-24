9e7f330a383acf91964560ffd53f448262941e39
GustavoHMeira <cc18015@g.unicamp.br>
2019-10-24 09:11:47 -0300

    falta verificações perfil

--- AppAndroidFotovoltaica/app/src/main/java/com/example/appandroidfotovoltaica/ui/perfil/PerfilFragment.java
+++ AppAndroidFotovoltaica/app/src/main/java/com/example/appandroidfotovoltaica/ui/perfil/PerfilFragment.java
@@ -1,5 +1,6 @@
 package com.example.appandroidfotovoltaica.ui.perfil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -13,25 +14,166 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.appandroidfotovoltaica.CalculadoraFotoVoltaica;
+import com.example.appandroidfotovoltaica.Cliente;
+import com.example.appandroidfotovoltaica.Empresa;
+import com.example.appandroidfotovoltaica.Enderecos;
+import com.example.appandroidfotovoltaica.MyTask;
 import com.example.appandroidfotovoltaica.R;
+import com.example.appandroidfotovoltaica.Usuario;
 import com.example.appandroidfotovoltaica.ValorMensalEnergia;
+import com.example.appandroidfotovoltaica.Verificadora;
+import com.example.appandroidfotovoltaica.ui.principalClientes.PrincipalClientesFragment;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PerfilFragment extends Fragment {
 
     private PerfilViewModel perfilViewModel;
+    EditText etNome, etTelefone, etData, etCpf, etSenhaUm, etSenhaConfirmada;
+    TextView tvEmail, tvEmpresa;
+    Button btnAlterar, btnExcluir;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         perfilViewModel =
                 ViewModelProviders.of(this).get(PerfilViewModel.class);
         View root = inflater.inflate(R.layout.fragment_perfil, container, false);
+        etNome = root.findViewById(R.id.etNomePerfil);
+        etTelefone = root.findViewById(R.id.etTelefonePerfil);
+        etData = root.findViewById(R.id.etDataPerfil);
+        etCpf = root.findViewById(R.id.etCpfPerfil);
+        etSenhaUm = root.findViewById(R.id.etSenhaUmPerfil);
+        etSenhaConfirmada = root.findViewById(R.id.etSenhaConfirmadaPerfil);
+        tvEmail = root.findViewById(R.id.tvEmailPerfil);
+        tvEmpresa = root.findViewById(R.id.tvEmpresaPerfil);
+        btnAlterar = root.findViewById(R.id.btnAlterarPerfil);
+        btnExcluir = root.findViewById(R.id.btnExcluirPerfil);
+        Intent intent = getActivity().getIntent();
+        final Usuario logado = (Usuario)intent.getSerializableExtra("usuario");
+        tvEmail.setText(logado.getEmail());
+        etSenhaUm.setText(logado.getSenha());
+        etSenhaConfirmada.setText(logado.getSenha());
+
+        MyTask task = new MyTask(Empresa[].class);
+        task.execute(Enderecos.GET_EMPRESAS + "_codigo/" + logado.getCodEmpresa());
+        while (task.isTrabalhando()) ;
+        Empresa[] resultEmpresas = (Empresa[]) task.getDados();
+
+        String nomeEmpresa = resultEmpresas[0].getNome();
+        tvEmpresa.setText(nomeEmpresa);
+
+        this.btnAlterar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final RequestQueue QUEUE = Volley.newRequestQueue(getActivity().getApplicationContext());
+
+                final String nome = etNome.getText().toString().trim();
+                final String data = etData.getText().toString();
+                final String telefone = etTelefone.getText().toString();
+                final String cpf = etCpf.getText().toString();
+                final String senha = etSenhaConfirmada.getText().toString();
+
+
+
+                StringRequest putRequest = new StringRequest(
+                        Request.Method.PATCH,
+                        Enderecos.PATCH_USUARIOS + "/" + logado.getCodigo(),
+                        new Response.Listener<String>()
+                        {
+                            @Override
+                            public void onResponse(String response) {
+                                // response
+                                Toast.makeText(
+                                        getActivity().getApplicationContext(),
+                                        "Perfil alterado",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        },
+                        new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // error
+                                Toast.makeText(
+                                        getActivity().getApplicationContext(),
+                                        "Erro ao alterar",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                )
+                {
+
+                    @Override
+                    protected Map<String, String> getParams()
+                    {
+                        Map<String, String>  params = new HashMap<String, String>();
+                        params.put("nome", nome);
+                        params.put("senha", senha);
+                        params.put("telefone", telefone);
+                        params.put("cpf", cpf);
+                        params.put("data", data);
+                        return params;
+                    }
+
+                };
+
+                QUEUE.add(putRequest);
+            }
+        });
+
+        this.btnExcluir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final RequestQueue QUEUE = Volley.newRequestQueue(getActivity().getApplicationContext());
+
+                StringRequest dr = new StringRequest(
+                        Request.Method.DELETE,
+                        Enderecos.DELETE_USUARIOS + "/" + logado.getCodigo(),
+                        new Response.Listener<String>()
+                        {
+                            @Override
+                            public void onResponse(String response) {
+                                // response
+                                Toast.makeText(
+                                        getActivity().getApplicationContext(),
+                                        "Usuário excluído",
+                                        Toast.LENGTH_SHORT).show();
+                                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                                //fragmentTransaction.replace(R.id.cliente_individual_fragment, new PrincipalClientesFragment());
+                                fragmentTransaction.commit();
+                            }
+                        },
+                        new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(
+                                        getActivity().getApplicationContext(),
+                                        "Erro ao excluir",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                );
+                QUEUE.add(dr);
+            }
+        });
 
         return root;
     }
+
 }
