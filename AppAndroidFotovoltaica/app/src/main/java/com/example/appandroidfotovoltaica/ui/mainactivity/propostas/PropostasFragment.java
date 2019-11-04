d25cf0153e1137f56d514cee0b92f2d748ca4d21
pedrogmo <42345539+pedrogmo@users.noreply.github.com>
2019-11-04 11:47:10 -0300

    Gera pdf

--- AppAndroidFotovoltaica/app/src/main/java/com/example/appandroidfotovoltaica/ui/mainactivity/propostas/PropostasFragment.java
+++ AppAndroidFotovoltaica/app/src/main/java/com/example/appandroidfotovoltaica/ui/mainactivity/propostas/PropostasFragment.java
@@ -1,10 +1,17 @@
 package com.example.appandroidfotovoltaica.ui.mainactivity.propostas;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -13,19 +20,109 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.appandroidfotovoltaica.R;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class PropostasFragment extends Fragment {
 
+    private static final int STORAGE_CODE = 1000;
+
     private PropostasViewModel propostasViewModel;
+
+    private Button btnGerar;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        propostasViewModel =
-                ViewModelProviders.of(this).get(PropostasViewModel.class);
+        this.propostasViewModel = ViewModelProviders.of(this).get(PropostasViewModel.class);
         View root = inflater.inflate(R.layout.fragment_propostas, container, false);
 
+        this.btnGerar = (Button) root.findViewById(R.id.btnGerarProposta);
+
+        this.btnGerar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M)
+                {
+                    if (getActivity().checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+                    {
+                        String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        requestPermissions(permissions, STORAGE_CODE);
+                    }
+                    else
+                    {
+                        savePdf();
+                    }
+                }
+                else
+                {
+
+                }
+            }
 
 
+        });
         return root;
+    }
+
+    private void savePdf()
+    {
+        Document doc = new Document();
+        String fileName = new SimpleDateFormat(
+            "yyyyMMdd_HHmmss",
+            Locale.getDefault()).format(System.currentTimeMillis());
+
+        String filePath = Environment.getExternalStorageDirectory() + "/" + fileName + ".pdf";
+
+        try
+        {
+            PdfWriter.getInstance(doc, new FileOutputStream(filePath));
+            doc.open();
+            String text = "O MEEIRO TEM UM GRANDE CORAÇÃO...";
+
+            doc.addAuthor("Gustavo de Meira");
+
+            doc.addTitle("Pdf do Meira");
+
+            doc.add(new Paragraph(text));
+
+            doc.close();
+
+            Toast.makeText(
+                getActivity().getApplicationContext(),
+                fileName + ".pdf saved to \n " + filePath,
+                Toast.LENGTH_SHORT).show();
+        }
+        catch(Exception exc)
+        {
+            Toast.makeText(
+                getActivity().getApplicationContext(),
+                exc.toString(),
+                Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode)
+        {
+            case STORAGE_CODE:{
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+
+                }
+                else
+                {
+                    Toast.makeText(
+                        getActivity().getApplicationContext(),
+                        "Permission denied.",
+                        Toast.LENGTH_SHORT).show();
+                }
+            }break;
+        }
     }
 }
